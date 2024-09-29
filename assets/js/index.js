7e1d141253c840387420d2b11d5c1f46fff9505e
David Dang <52668181+dvkdang@users.noreply.github.com>
2024-09-29 23:11:17 +0100

    v4 - pathing to webpages changed
    v4 - added '/minigames/' to each if statement to hopefully fix routing.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,24 @@
 //grabs id from clickable button to add to new html via relative file path.
 //v3 - should work hopefully
-$(".button-click").click(function() {
-    var id = $(this).attr("id");
-    window.location.pathname='minigames/' + id + '.html';
-})
+// $(".button-click").click(function() {
+//     var id = $(this).attr("id");
+//     window.location.pathname='minigames/' + id + '.html';
+// })
 
-// v2
+// v4 -> included 'minigames/'
 // alternative path finder (didn't work)
 
-// const simonButton = document.getElementById("simon");
-// const clickerButton = document.getElementById("clicker");
+const simonButton = document.getElementById("simon");
+const clickerButton = document.getElementById("clicker");
 
-// if (simonButton) {
-//     simonButton.addEventListener('click', () => {
-//         window.location.pathname = '/simon.html';
-//     });
-// }
+if (simonButton) {
+    simonButton.addEventListener('click', () => {
+        window.location.pathname = '/minigames/simon.html';
+    });
+}
 
-// if (clickerButton) {
-//     clickerButton.addEventListener('click', () => {
-//         window.location.pathname = '/clicker.html';
-//     });
-// }
+if (clickerButton) {
+    clickerButton.addEventListener('click', () => {
+        window.location.pathname = '/minigames/clicker.html';
+    });
+}
