4ce263faf4e7fbd7d22027810de13a0a2eacf0c8
David Dang <52668181+dvkdang@users.noreply.github.com>
2024-09-29 22:32:44 +0100

    v3 - hopefully should work with /minigames root

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,24 @@
-// grabs id from clickable button to add to new html via relative file path.
-// didn't work in v1, look at v2
-// $(".button-click").click(function() {
-//     var id = $(this).attr("id");
-//     window.location.pathname='./' + id + '.html';
-// })
+//grabs id from clickable button to add to new html via relative file path.
+//v3 - should work hopefully
+$(".button-click").click(function() {
+    var id = $(this).attr("id");
+    window.location.pathname='minigames/' + id + '.html';
+})
 
 // v2
-// alternative path finder
+// alternative path finder (didn't work)
 
-const simonButton = document.getElementById("simon");
-const clickerButton = document.getElementById("clicker");
+// const simonButton = document.getElementById("simon");
+// const clickerButton = document.getElementById("clicker");
 
-if (simonButton) {
-    simonButton.addEventListener('click', () => {
-        window.location.pathname = '/simon.html';
-    });
-}
+// if (simonButton) {
+//     simonButton.addEventListener('click', () => {
+//         window.location.pathname = '/simon.html';
+//     });
+// }
 
-if (clickerButton) {
-    clickerButton.addEventListener('click', () => {
-        window.location.pathname = '/clicker.html';
-    });
-}
+// if (clickerButton) {
+//     clickerButton.addEventListener('click', () => {
+//         window.location.pathname = '/clicker.html';
+//     });
+// }
