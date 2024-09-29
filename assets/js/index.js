7048c286e0c64db9f41f6daaf6732aee2fc10a6e
David Dang <52668181+dvkdang@users.noreply.github.com>
2024-09-29 22:26:39 +0100

    updated index.js to correct file pathing

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,24 @@
 // grabs id from clickable button to add to new html via relative file path.
-$(".button-click").click(function() {
-    var id = $(this).attr("id");
-    window.location.pathname='./' + id + '.html';
-})
+// didn't work in v1, look at v2
+// $(".button-click").click(function() {
+//     var id = $(this).attr("id");
+//     window.location.pathname='./' + id + '.html';
+// })
 
+// v2
 // alternative path finder
 
-// const simonButton = document.getElementById("simon");
-// const clickerButton = document.getElementById("clicker");
+const simonButton = document.getElementById("simon");
+const clickerButton = document.getElementById("clicker");
 
-// if (simonButton) {
-//     simonButton.addEventListener('click', () => {
-//         window.location.pathname = '/simon.html';
-//     });
-// }
+if (simonButton) {
+    simonButton.addEventListener('click', () => {
+        window.location.pathname = '/simon.html';
+    });
+}
 
-// if (clickerButton) {
-//     clickerButton.addEventListener('click', () => {
-//         window.location.pathname = '/clicker.html';
-//     });
-// }
+if (clickerButton) {
+    clickerButton.addEventListener('click', () => {
+        window.location.pathname = '/clicker.html';
+    });
+}
